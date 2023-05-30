c558249a3f9da3e139da0c130b4ce0e6b9fffbfb
Daitomio <dumiao920216@qq.com>
2023-05-30 16:31:43 +0800

    添加项目文件。

---
+++ Controllers/MioBotController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+using MioBotAPI.Socket;
+
+namespace MioBotAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class MioBotController : Controller
+    {
+        [HttpGet]
+        public ActionResult<JsonObject> Get()
+        {
+            var jsontext = "{\"Hello\":\"World\"}";
+            return JsonNode.Parse(jsontext)!.AsObject(); ;
+        }
+        [HttpPost]
+        public ActionResult<JsonObject> Post(string msg,string qq,string group)
+        {
+            string str = msg + "|" + qq + "|" + group;
+            Client.Send(str);
+            var jsontext = "{\"success\":\"true\"}";
+            return JsonNode.Parse(jsontext)!.AsObject(); ;
+        }
+    }
+}
---
+++ Socket/Client.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MioBotAPI.Socket
+{
+    public class Client
+    {
+        static TcpClient client = new();
+        public static void Connect()
+        {
+            var config = new ConfigurationBuilder()
+                             .SetBasePath(Environment.CurrentDirectory)
+                             .AddJsonFile("appsettings.json")
+                             .AddInMemoryCollection()
+                             .Build();
+            var ip = IPAddress.Parse(config["Address:Ip"]);
+            var port = Convert.ToInt32(config["Address:Port"]);
+            IPEndPoint address = new(ip, port);
+            client.Connect(address);
+        }
+        public static void Send(string msg)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] data = Encoding.Default.GetBytes(msg);
+            stream.Write(data, 0, data.Length);
+        }
+    }
+}
